3a611fb85609b28cc9fec02d7f60a7acac9d9b68
Prasanna RV <prasannavelmurugan0200@gmail.com>
2021-11-28 15:27:16 +0530

    changes

--- public/JS/about.js
+++ public/JS/about.js
@@ -17,10 +17,67 @@ gsap.to(sections, {
 });
 
 
+
 gsap
     .timeline({
         scrollTrigger: {
-            trigger: ".images-speakers-main-div",
-            start: "top 40%"
+            trigger: '.image-speaker',
         }
     })
+    .to('.image-speaker', {
+        duration: 1,
+        y: 0,
+        stagger: .1,
+    })
+
+
+
+const imagebelow = (divimage, width, height, top, bottom, right) => {
+    gsap
+        .timeline({
+            scrollTrigger: {
+                trigger: '.image-organizer',
+                start: 'top 40%'
+            }
+        })
+        .to(divimage, {
+            duration: 1,
+            width: width,
+            height: height,
+            top: top,
+            bottom: bottom,
+            right: right,
+            stagger: .1,
+        })
+}
+
+imagebelow('.red-div', '23%', '4rem', '-4%', "none", '5%');
+imagebelow('.img-organizer', '65%', '13rem', "none", '6%', 'none');
+imagebelow('.grey-div', '30%', '4rem', "50%", 'none', '13%');
+
+
+
+const mainimage = (allimage, width, height, top, bottom, left, right) => {
+    gsap
+        .timeline({
+            scrollTrigger: {
+                trigger: '.image-border-right',
+                start: 'top 40%'
+            }
+        })
+        .to(allimage, {
+            duration: 1,
+            width: width,
+            height: height,
+            top: top,
+            bottom: bottom,
+            left: left,
+            right: right,
+            stagger: .1,
+        })
+}
+
+mainimage(".image-border1", "50%", "18rem", "5%", "none", "none", "20%");
+mainimage(".image-border2", "45%", "15rem", "30%", "none", "none", "0%");
+mainimage(".image-border-dot1", "none", "none", "-6%", "none", "25%", "none");
+mainimage(".image-border-dot2", "none", "none", "52%", "none", "none", "-5%");
