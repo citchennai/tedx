a00b0e6fb67085f83f48446f0f561965ba554edd
solomonsanthosh <solosanty2064@gmail.com>
2021-12-22 21:44:51 +0530

    SS

--- public/JS/guideline.js
+++ public/JS/guideline.js
@@ -13,3 +13,26 @@ Btn.forEach(e => {
     })
 })
 
+
+const readbtn = document.querySelectorAll('.readMore')
+const contentBox = document.querySelectorAll('.pAll')
+readbtn.forEach(btn => {
+    btn.addEventListener('click',e => {
+        const count = e.target.getAttribute('count')
+        contentBox[parseInt(count)].classList.toggle('active')
+
+        console.log(contentBox[parseInt(count)].getAttribute('clicked'));
+        if(contentBox[parseInt(count)].getAttribute('clicked') == 'false') {
+            e.target.innerHTML = 'Read less'
+            contentBox[parseInt(count)].style.height = 'auto'
+            contentBox[parseInt(count)].setAttribute('clicked','true')
+            console.log(contentBox[parseInt(count)].getAttribute('clicked'));
+        } else {
+            contentBox[parseInt(count)].style.height = '250px'
+            e.target.innerHTML = 'Read more'
+            contentBox[parseInt(count)].setAttribute('clicked','false')
+        }
+    
+        
+    })
+})
