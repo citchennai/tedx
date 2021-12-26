0d87d3d0b091aee62627d619b2775b523c3252f9
Aravindhabii <abiiaravindhr.it2020@citchennai.net>
2021-12-26 11:55:26 +0530

    oruspeakers

---
+++ public/JS/ourspeakers.js
@@ -0,0 +1,53 @@
+const paginum = (page)=>{
+    $("#pagination-container").pagination({
+        dataSource: function (done) {
+          var result = [];
+          for (var i = 1; i < 34; i++) {
+            result.push("image"+i);
+          }
+          done(result);
+        },
+        locator: "items",
+        totalNumberLocator: function (response) {
+          // you can return totalNumber by analyzing response content
+          return Math.floor(Math.random() * (1000 - 100)) + 100;
+        },
+        pageSize: page,
+        className: 'paginationjs-theme-blue paginationjs-big',
+        ajax: {
+          beforeSend: function () {
+            $("#data-container").html("Loading images from TEDx...");
+          },
+        },
+        callback: function (data, pagination) {
+          var html = "";
+          $.each(data, function (index, detail) {
+            html += `<div class="gridbox-${index} girdbox">
+                      <img src="../public/images/ourspeakers/speakers/${detail}.jpg" class="speakerimg" alt="image">
+                      </div>`;
+          });
+          $("#data-container").html(html);
+        },
+      });
+}
+
+window.addEventListener("resize",()=>{
+    if (window.screen.width < 920) {
+        paginum(8)
+    }else{
+        paginum(9)
+    }
+})
+
+if (window.screen.width < 920) {
+    paginum(8)
+}else{
+    paginum(9)
+}
+
+
+
+
+
+
+
